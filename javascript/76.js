e2253bea961154bf6f14038e2de5063d0ae817e3
dhananhub <dkjha444@gmail.com>
2023-12-29 22:47:42 +0530

    add

---
+++ javascript/76.js
@@ -0,0 +1,14 @@
+// generator function
+function* generatorFunc() {
+  
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const obj = generatorFunc();
+
+// iteration through generator
+for (let value of obj) {
+    console.log(value);
+}
