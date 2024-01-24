9fe002f2c6d3c048d8709c9fef6025e6172b046e
dhananhub <dkjha444@gmail.com>
2024-01-24 23:41:38 +0530

    add

---
+++ javascript/100.js
@@ -0,0 +1,14 @@
+
+const person = {
+    name: 'John',
+    age: 21,
+}
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
