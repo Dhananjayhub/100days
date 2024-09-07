3dfa1774b3565eda5428f31af760e9bfe6f41284
Dhananjayhub <dkjha444@gmail.com>
2024-09-07 23:23:34 +0530

    add

--- javascript/41.js
+++ javascript/41.js
@@ -14,3 +14,49 @@ const generator = generatorFunc();
 console.log(generator.next());
 console.log(generator.next());
 console.log(generator.next());
+
+
+
+
+
+
+
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+
+
+}// program that checks if the number is positive, negative or zero
+// input from the user
+const number1 = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+
+     
+}
