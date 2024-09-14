bd8ae18a9a130059f6e03a0a1dab0ddca5dc6d6e
Dhananjayhub <dkjha444@gmail.com>
2024-09-14 22:07:13 +0530

    add

--- javascript/28.js
+++ javascript/28.js
@@ -15,3 +15,192 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
