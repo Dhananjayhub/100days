aadab6316e4e11f56deb8b312326df07e160e081
Dhananjayhub <dkjha444@gmail.com>
2024-10-11 22:24:18 +0530

    dvsd

--- javascript/28.js
+++ javascript/28.js
@@ -16849,3 +16849,40 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+
+nsole.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(person.name);
+// program to clone the object
+// declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
