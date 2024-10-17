57cb540aecdec9ca0807b6668f36ccc4c73982b1
Dhananjayhub <dkjha444@gmail.com>
2024-10-17 22:13:32 +0530

    fwga

--- javascript/28.js
+++ javascript/28.js
@@ -16917,4 +16917,58 @@ console.log(clonePerson);
 clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
+console.log(clonePerson.name);
+console.log(person.name);
+/ declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(clonePerson.name);
+console.log(person.name);
+/ declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(clonePerson.name);
+console.log(person.name);
+/ declaring object
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+// cloning the object
+const clonePerson = { ... person}
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
 console.log(person.name);
