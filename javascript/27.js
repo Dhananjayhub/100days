d5734bb7c078e3234339629c7c2f2008a600fc3e
Dhananjayhub <dkjha444@gmail.com>
2024-10-20 23:26:59 +0530

    fdfsag

--- javascript/27.js
+++ javascript/27.js
@@ -221,3 +221,39 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
