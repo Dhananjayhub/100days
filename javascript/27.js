5d7361de3f7fa8a0c63b2d565bb38b1e8bf5b9dd
Dhananjayhub <dkjha444@gmail.com>
2024-10-22 22:10:29 +0530

    dwrewr

--- javascript/27.js
+++ javascript/27.js
@@ -455,3 +455,71 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+sole.log(person.name);
+onsole.log(person.name);
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+sole.log(person.name);
+onsole.log(person.name);
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+sole.log(person.name);
+onsole.log(person.name);
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+sole.log(person.name);
+onsole.log(person.name);
+
+console.log(clonePerson.name);
+console.log(person.name);
+nsole.log(clonePerson.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
