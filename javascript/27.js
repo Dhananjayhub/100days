4433cee25b861c4075a1da5100b7483c2ff18015
Dhananjayhub <dkjha444@gmail.com>
2024-11-27 09:57:35 +0530

    dvsdv

--- javascript/27.js
+++ javascript/27.js
@@ -1426,3 +1426,35 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(person.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+
+
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(person.name);
+console.log(person.name);
+// cloning the object
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
