211a67a1d083ed57cd39ad812edf79568de94d0c
Dhananjayhub <dkjha444@gmail.com>
2024-11-30 21:44:09 +0530

    rege

--- javascript/27.js
+++ javascript/27.js
@@ -1563,3 +1563,36 @@ clonePerson.name = 'Peter';
 
 console.log(clonePerson.name);
 console.log(person.name);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(clonePerson.name);
+console.log(person.name);
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
+console.log(clonePerson.name);
+console.log(person.name);
+const clonePerson = Object.assign({}, person);
+
+console.log(clonePerson);
+
+// changing the value of clonePerson
+clonePerson.name = 'Peter';
+
+console.log(clonePerson.name);
+console.log(person.name);
